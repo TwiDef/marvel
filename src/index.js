c53c2ecc8dc3b859eae8048b06d6bb9de8d56188
Def <deferenseo@gmail.com>
2023-09-22 13:08:06 +0300

    setted query resources

--- src/index.js
+++ src/index.js
@@ -2,11 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import MarvelService from './services/MarvelService';
+
+const marvelService = new MarvelService()
+marvelService.getAllCharacters().then(res => res.data.results.forEach(item => {
+    console.log(item.name)
+}))
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-
     <App />
-
 );
 
